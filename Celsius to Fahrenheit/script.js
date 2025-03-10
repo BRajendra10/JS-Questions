c725f7fff93f5191523a2421038d40dc5c17a92f
Rajendra Behera <rajendrabehera8116@gmail.com>
2025-03-10 06:45:02 +0530

    Delete Celsius to Fahrenheit/script.js

--- Celsius to Fahrenheit/script.js
+++
@@ -1,8 +0,0 @@
-// WAP to converte Celsius to Fahrenheit
-
-let celsius = prompt("Enter a number: ");
-celsius = parseInt(celsius);
-
-const fahrenheit = celsius * 1.8 + 32;
-
-console.log(`${celsius} deg celsius is = ${fahrenheit} fahrenheit.`);
